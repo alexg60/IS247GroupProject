a533d9f05e1b379f0d9197a6fca43e0e9aeab4a6
alexgilbert <alexg6@umbc.edu>
2025-02-17 18:46:06 -0500

    Added a message to make sure i can commit and push

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("I got git to work");
+        System.out.println("This is another message");
     }
 }
